bdd9020471dfe0dd279a38119e1329b16fd085ba
Luch <lperotti39@gmail.com>
2025-06-10 17:22:32 -0400

    Uploading portfolio  build

---
+++ src/assets/services_data.js
@@ -0,0 +1,40 @@
+const Services_Data = [
+  {
+    s_no: "01",
+    s_name: "Native App Development",
+    s_desc:
+      "Building apps specifically for iOS (Swift) platform, maximizing performance and utilizing native features.",
+  },
+  {
+    s_no: "02",
+    s_name: "Cross-Platform App Development",
+    s_desc:
+      "Creating apps that can run on both iOS and Android using the React Native and Expo platforms.",
+  },
+  {
+    s_no: "03",
+    s_name: "Maintenance and Support",
+    s_desc:
+      "Providing ongoing support, updates, and bug fixes for the mobile applications.",
+  },
+  {
+    s_no: "04",
+    s_name: "App Testing and MVP Development",
+    s_desc:
+      "Thoroughly testing the app on various devices and platforms to ensure functionality, performance, and stability.  Creating a minimal viable product (MVP) to test the app's viability before full development.",
+  },
+  {
+    s_no: "05",
+    s_name: "Performance Optimization and App Store Deployment",
+    s_desc:
+      "Optimizing the app for speed, memory usage, and battery life.  Deploying apps to Apple App Store and Google Play Store, ensuring App Store compliance requirements.",
+  },
+  {
+    s_no: "06",
+    s_name: "Content Writing",
+    s_desc:
+      "Producing technical documentation for an application’s users and developers.  Optimizing the development process through training and enablement materials.",
+  },
+];
+ 
+export default Services_Data;
